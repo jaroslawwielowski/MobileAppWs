dda1fed9ed11aecee75089b21bd830dc1c6616b9
Jarosław Wielowski <jaroslawwielowski@gmail.com>
2019-05-09 17:10:08 +0200

    next get with page and limit

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -6,15 +6,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 	
+	@GetMapping
+	public String getUsers(@RequestParam(value="page") int page, 
+			@RequestParam(value="limit") int limit) {
+		return "get user was called with pahe " + page + " and limit " + limit;
+	}
+	
 	@GetMapping(path="/{userId}")
 	public String getUser(@PathVariable String userId) {
-		return "get user was called with userId " + userId;
+		return "get user was called with userId : " + userId;
 	}
 	
 	
