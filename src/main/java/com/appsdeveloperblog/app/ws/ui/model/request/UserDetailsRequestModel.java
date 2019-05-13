a87953f311a882e60484b96ec67551752a0df9ae
Jarosław Wielowski <jaroslawwielowski@gmail.com>
2019-05-13 19:48:51 +0200

    validation message first and last name

--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -1,18 +1,23 @@
 package com.appsdeveloperblog.app.ws.ui.model.request;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserDetailsRequestModel {
 
-	@NotNull(message="firstname cannot be null")
+	@NotNull(message = "firstname cannot be null")
+	@Size(min = 2, message="firstname must hev minimum 2 characters")
 	private String firstName;
-	@NotNull(message="lastname cannot be null")
+	@NotNull(message = "lastname cannot be null")
+	@Size(min = 2, message="lasttname must hev minimum 2 characters")
+
 	private String lastName;
-	@NotNull(message="email cannot be null")
+	@NotNull(message = "email cannot be null")
+	@Email
 	private String email;
-	@NotNull(message="password name cannot be null")
-	@Size(min=8, max=30, message="password must be betwen 8 and 30 characters")
+	@NotNull(message = "password name cannot be null")
+	@Size(min = 8, max = 30, message = "password must be betwen 8 and 30 characters")
 	private String password;
 
 	public String getFirstName() {
