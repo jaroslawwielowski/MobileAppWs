1c283172a8a2c8d55c54c75b50e586011c3b853b
Jarosław Wielowski <jaroslawwielowski@gmail.com>
2019-08-04 15:16:41 +0200

    last today

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
 	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 
-//		if(true) throw new UserServiceExeption("A user service exeption is thrown");
+		if(true) throw new UserServiceExeption("A user service exeption is thrown");
 		
 		if (users.containsKey(userId)) {
 			return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
